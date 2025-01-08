22c23829f8487a08c02b4f76927dfcda6fad88d9
Maithil_Sangani65 <patelmaithil65@gmail.com>
2025-01-08 13:09:06 +0530

    Add files via upload

---
+++ resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-resume',
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+
+}
